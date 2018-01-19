f7339e56b58e27fe87109233a12f090d3f6abf15
Hunter Caron <huntercaron@mac.com>
2018-01-19 13:36:40 -0500

    adding basic map

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
+import ReactMapGL from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 // styled components
 const Container = styled.div`
@@ -10,10 +12,25 @@ const Text = styled.p`
 `
 
 // component
-export default function Map(props) {
-  return (
-    <Container>
-      <Text>{props.text}</Text>
-    </Container>
-  )
+export default class Map extends React.Component {
+  state = {
+    viewport: {
+      width: 400,
+      height: 200,
+      latitude: 43.63683,
+      longitude: -79.40841,
+      zoom: 8
+    }
+  };
+
+  render() {
+    return (
+      <Container>
+        <ReactMapGL
+        {...this.state.viewport}
+        onViewportChange={(viewport) => this.setState({viewport})}
+        />
+      </Container>
+    )
+  }
 }
